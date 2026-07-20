7d33327392543e71258db8d4b5b16c758553ff9f
Sealybla <lijialin2018@gmail.com>
2026-07-20 19:37:33 +0000

    created basic modules and dune project

---
+++ lib/card.ml
@@ -0,0 +1,28 @@
+open! Core
+
+module Color = struct
+  type t = 
+  | Red
+  | Green
+  | Blue
+  | Yellow
+  | NoColor
+
+end
+
+module Effect = struct
+  type t = 
+| Skip
+| Plus
+| Reverse
+| NoEffect
+| Wild
+| Wild4
+
+end
+
+type t = 
+{ 
+  color:Color.t;
+  effect: Effect.t
+}[@@deriving sexp_of]
---
+++ lib/card.mli
@@ -0,0 +1,16 @@
+open! Core
+
+type t [@@deriving sexp_of]
+
+module Color: sig
+end
+
+
+module Effect : sig
+  
+end
+
+
+(* 
+
+val color: t -> Color.t *)
